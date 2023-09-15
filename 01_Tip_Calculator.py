b0b4381f72a6ea10b603a87635e717a74d453585
DongokYang <dyang2@academic.rrc.ca>
2023-09-15 09:41:00 -0500

    Udemy 100days of python

---
+++ 01_Tip_Calculator.py
@@ -0,0 +1,12 @@
+"""
+Description: Simple tip calculator
+Author: Dongok Yang
+Date: 2023.09.15
+"""
+
+print("Welcome to the tip calculator.")
+total_bill = float(input("What was the total bill? $"))
+percentage = int(input("What percentage tip would you like to give? 10, 12, or 15?"))
+people = int(input("How many people to split the bill?"))
+money = total_bill*(100+percentage)/100/people
+print(f"Each person should pay:{round(money,2)}")
